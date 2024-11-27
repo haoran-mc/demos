b69bca1dc7b094dd0fc72a898fb6d7b4bdb3c9f1
haoran <haoran.mc@outlook.com>
2024-11-27 21:05:29 +0800

    gin echo server

--- go-gin-echo/main.go
+++ go-gin-echo/main.go
@@ -6,12 +6,22 @@ func main() {
 	r := gin.Default()
 	r.GET("/*path", func(ctx *gin.Context) {
 		p := ctx.Param("path")
-		ctx.String(200, "Path: %s\n", p)
+		ctx.String(200, "[GET] Path: %s\n", p)
 	})
 	r.POST("/*path", func(ctx *gin.Context) {
 		p := ctx.Param("path")
 		raw, _ := ctx.GetRawData()
-		ctx.String(200, "Path: %s, Data: %s\n", p, string(raw))
+		ctx.String(200, "[POST] Path: %s, Data: %s\n", p, string(raw))
+	})
+	r.PUT("/*path", func(ctx *gin.Context) {
+		p := ctx.Param("path")
+		raw, _ := ctx.GetRawData()
+		ctx.String(200, "[PUT] Path: %s, Data: %s\n", p, string(raw))
+	})
+	r.DELETE("/*path", func(ctx *gin.Context) {
+		p := ctx.Param("path")
+		raw, _ := ctx.GetRawData()
+		ctx.String(200, "[DELETE] Path: %s, Data: %s\n", p, string(raw))
 	})
 	r.Run(":9520")
 }
