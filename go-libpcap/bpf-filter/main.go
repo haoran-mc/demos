298459d270d595dbd6bb1b85aa7751dfe34eb86d
haoran <haoran.mc@outlook.com>
2025-01-23 20:32:14 +0800

    gopacket set bpf-filter

---
+++ go-libpcap/bpf-filter/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+var (
+	device       string = "en0"
+	snapshot_len int32  = 1024
+	promiscuous  bool   = false
+	err          error
+	timeout      time.Duration = 30 * time.Second
+	handle       *pcap.Handle
+)
+
+func main() {
+	// 打开网卡设备
+	handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	filterStr := "tcp and port 80"
+	err = handle.SetBPFFilter(filterStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Only capturing TCP port 80 packets.")
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		// Do something with a packet here.
+		fmt.Println(packet)
+	}
+}
