9916a043de9ec317a9f8ec40acc01c17080b672c
haoran <haoran.mc@outlook.com>
2025-02-08 12:35:40 +0800

    print a full packet

--- go-libpcap/print-pkt/main.go
+++ go-libpcap/print-pkt/main.go
@@ -46,10 +46,12 @@ func main() {
 	fmt.Println(ipPacket)
 	fmt.Println(tcpPacket)
 
-	fullPacket() // 完整的 Packet
+	pkt := createFullPacket() // 构造一个完整的 Packet
+	fmt.Println(pkt)
+	fmt.Println(forgeReset(pkt)) // 操作 Packet
 }
 
-func fullPacket() {
+func createFullPacket() gopacket.Packet {
 	// 构造 Ethernet 层（数据链路层，网络七层模型中的第二层）
 	ethLayer := &layers.Ethernet{
 		SrcMAC:       net.HardwareAddr{0x00, 0x0c, 0x29, 0x2d, 0x8d, 0x21}, // 源网卡 MAC 地址
@@ -96,7 +98,60 @@ func fullPacket() {
 		log.Printf("序列化失败: %v", err)
 	}
 
-	// 发送数据包
-	resetPacket := gopacket.NewPacket(buffer.Bytes(), layers.LayerTypeEthernet, gopacket.Default)
-	fmt.Println(resetPacket)
+	newPacket := gopacket.NewPacket(buffer.Bytes(), layers.LayerTypeEthernet, gopacket.Default)
+	return newPacket
+}
+
+func forgeReset(packet gopacket.Packet) gopacket.Packet {
+	ethLayer := packet.Layer(layers.LayerTypeEthernet)
+	eth, _ := ethLayer.(*layers.Ethernet)
+
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	ip, _ := ipLayer.(*layers.IPv4)
+
+	tcpLayer := packet.Layer(layers.LayerTypeTCP)
+	tcp, _ := tcpLayer.(*layers.TCP)
+
+	options := gopacket.SerializeOptions{
+		ComputeChecksums: true,
+		FixLengths:       true,
+	}
+
+	tcp.RST = true
+	tcp.NS = false
+	tcp.CWR = false
+	tcp.ECE = false
+	tcp.URG = false
+	tcp.PSH = false
+	tcp.SYN = false
+	tcp.FIN = false
+
+	if tcp.ACK {
+		tcp.ACK = false
+		tcp.Seq = tcp.Ack
+		tcp.Ack = 0
+	} else {
+		tcp.ACK = true
+		tcp.Ack = tcp.Seq + uint32(len(packet.Data()))
+		tcp.Seq = 0
+	}
+
+	tcp.Window = 0
+	tcp.Urgent = 0
+	tcp.Options = tcp.Options[:0]
+	tcp.Payload = tcp.Payload[:0]
+
+	tcp.SrcPort, tcp.DstPort = tcp.DstPort, tcp.SrcPort
+	ip.SrcIP, ip.DstIP = ip.DstIP, ip.SrcIP
+	eth.SrcMAC, eth.DstMAC = eth.DstMAC, eth.SrcMAC
+
+	tcp.SetNetworkLayerForChecksum(ip)
+
+	resetPacketBuffer := gopacket.NewSerializeBuffer()
+	err := gopacket.SerializePacket(resetPacketBuffer, options, packet)
+	if err != nil {
+		panic(err)
+	}
+	resetPacket := gopacket.NewPacket(resetPacketBuffer.Bytes(), layers.LayerTypeEthernet, gopacket.Default)
+	return resetPacket
 }
